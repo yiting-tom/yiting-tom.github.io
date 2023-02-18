d6be743bab8211e60f1ba650b1d2548aa98cd9e5
yiting-tom <liyitingtom@gmail.com>
2023-02-18 23:05:14 +0800

    feat: complete experiences section

--- src/res/data/experience.js
+++ src/res/data/experience.js
@@ -3,20 +3,26 @@ export const experiences = [
       src: require("assets/img/experiences/analysis.png").default,
       title: "Data Analysis",
       category: "Internship",
-      desp: "I had two years of data analytics intern experience while in college"
+      desp: "I had two years of data analytics intern experience while in college" +
+      "through which I learned how to use SQL, Python, Pandas and Tableau to analyze data and build reports. " +
+      "I also designed and built a customer behavior analysis pipeline, platform and dashboard for a company." 
     },    
     {
-      src: require("assets/img/icons/d_cs.png").default,
+      src: require("assets/img/experiences/web.png").default,
       title: "Software Developer",
       category: "Internship",
-      desp: "Due to my passion for programming, I am engaged in software development, have been exposed to many development and maintenance projects, love technology, and continue to learn."
+      desp: "I love to inspect and understand the inner workings of software and build my own. "+
+      "From designing, developing, testing, and deploying, I have a full stack of experience in software development." +
+      "And I'm also a big fan of cloud computing and DevOps, so in my free time I also learn about Terraform, Kubernetes, and CI/CD."
     },
     {
-      src: require("assets/img/icons/ms_cs.png").default,
+      src: require("assets/img/experiences/ai.png").default,
       title: "NLP & Cross-modal ML",
       category: "Master's Degree",
-      desp: "Now I'm studying AI on Recommender systems, Time Series forecasting, model interpretability, and NLP. "+
-      "Implement and use AI models on real industry data to solve complex business problems and help people make better decisions."
+      desp: "I'm currently pursuing a master's degree in CS "+
+      "with a focus on NLP and explainablilty of Cross-modal Visual Language models. "+
+      "Now I'm working on a project that aims to leverage the power of NLP and CV to " +
+      "build a monitor for the Google Pixel Phones testing and marketing team to trace user sentiment"
     },
 ];
 
